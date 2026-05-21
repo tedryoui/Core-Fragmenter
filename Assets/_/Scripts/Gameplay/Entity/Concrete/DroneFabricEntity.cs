db0dc932e9104d32a3f317095f6ce1a5e5f5cba2
tedryoui <yauheni.tushinskiy@gmail.com>
2026-05-21 18:10:26 +0300

    Added + Drone Fabric Idle State Changed * Drone Fabric Entity * Gameplay Scene

--- Assets/_/Scripts/Gameplay/Entity/Concrete/DroneFabricEntity.cs
+++ Assets/_/Scripts/Gameplay/Entity/Concrete/DroneFabricEntity.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using _.Scripts.Data.Concrete;
+using _.Scripts.Gameplay.Entity.Concrete.Dron_Fabric;
+using _.Scripts.Gameplay.Player;
 using _.Scripts.Scriptable_Objects;
 using _.Scripts.Services;
+using _.Scripts.User_Interface;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using VContainer;
@@ -12,17 +15,27 @@ namespace _.Scripts.Gameplay.Entity.Concrete
     public class DroneFabricEntity : MonoEntity
     {
         [Inject] private ServiceLocator _serviceLocator;
+        [Inject] private PlayerProfile  _playerProfile;
         
         public override string              Identity       => _identity;
-        public override List<AbstractState> PossibleStates { get; }
+        public override List<AbstractState> PossibleStates =>  new List<AbstractState>()
+        {
+            new DroneFabricIdleState(this)
+        };
         
         [SerializeField] private string _identity;
+        
+        [SerializeField] private WorldUtilityWindowViewModel _worldUtilityWindowViewModel;
+        public WorldUtilityWindowViewModel WorldUtilityWindowViewModel => _worldUtilityWindowViewModel;
 
         private DataService _dataService;
         public DataService DataService => _dataService ??= _serviceLocator.Get<DataService>();
         
         private DroneFabricData _droneFabricData;
         public DroneFabricData DroneFabricData => _droneFabricData ??= DataService.Get<DroneFabricData>(Identity);
+        
+        private PlayerData _playerData;
+        public  PlayerData PlayerData => _playerData ??= DataService.Get<PlayerData>(_playerProfile.ID);
         
         [SerializeField, ReadOnly] private List<BlueprintScriptableObject> _blueprints;
 
---
+++ Assets/_/Scripts/Gameplay/Entity/State/Concrete/Dron Fabric/DroneFabricIdleState.cs
@@ -0,0 +1,97 @@
+﻿using System.Linq;
+using _.Scripts.User_Interface;
+using Unity.Mathematics;
+
+namespace _.Scripts.Gameplay.Entity.Concrete.Dron_Fabric
+{
+    public class DroneFabricIdleState : AbstractState<DroneFabricEntity>
+    {
+        public override string Identity => "Drone Fabric Idle";
+
+        public DroneFabricIdleState(DroneFabricEntity entity) : base(entity)
+        {
+        }
+
+        private         bool   IsUIActive => Entity.WorldUtilityWindowViewModel.IsVisible();
+
+        public override void OnEnter()
+        {
+            SetupUI();
+            
+            base.OnEnter();
+        }
+
+        private void SetupUI()
+        {
+            Entity.WorldUtilityWindowViewModel.ConfigureButton("Manage!", false);
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+
+            if (DoesPlayerInRadius())
+            {
+                if (!IsUIActive)
+                    ShowUI();
+                UpdateUI();
+            }
+            else
+            {
+                if (IsUIActive)
+                    HideUI();
+            }
+        }
+
+        private bool DoesPlayerInRadius()
+        {
+            var distance = math.distance(Entity.transform.position, Entity.PlayerData.Position);
+            
+            return distance <= 3.0f;
+        }
+
+        private void UpdateUI()
+        {
+            var isButtonActive =
+                Entity.WorldUtilityWindowViewModel.IsBitEnabled(WorldUtilityWindowViewModel.UtilityBit.Button);
+            var isOutOfOrderLimit = Entity.DroneFabricData.ActiveOrders.Count >= Entity.DroneFabricData.MaxActiveOrders;
+            
+            if (isButtonActive && isOutOfOrderLimit)
+            {
+                Entity.WorldUtilityWindowViewModel.SetBitVisible(WorldUtilityWindowViewModel.UtilityBit.Button, false);
+                Entity.WorldUtilityWindowViewModel.SetButtonInteractable(false);
+            }
+            else if (!isButtonActive && !isOutOfOrderLimit)
+            {
+                Entity.WorldUtilityWindowViewModel.SetBitVisible(WorldUtilityWindowViewModel.UtilityBit.Button, true);
+                Entity.WorldUtilityWindowViewModel.SetButtonInteractable(true);
+            }
+
+            var isTimerActive = Entity.WorldUtilityWindowViewModel.IsBitEnabled(WorldUtilityWindowViewModel.UtilityBit.Timer);
+            var hasOrder = Entity.DroneFabricData.ActiveOrders.Count != 0;
+            
+            if (isTimerActive && !hasOrder)
+                Entity.WorldUtilityWindowViewModel.SetBitVisible(WorldUtilityWindowViewModel.UtilityBit.Timer, false);
+            else if (!isTimerActive && hasOrder)
+                Entity.WorldUtilityWindowViewModel.SetBitVisible(WorldUtilityWindowViewModel.UtilityBit.Timer, true);
+            
+            isTimerActive = Entity.WorldUtilityWindowViewModel.IsBitEnabled(WorldUtilityWindowViewModel.UtilityBit.Timer);
+            
+            if (isTimerActive)
+            {
+                var first = Entity.DroneFabricData.ActiveOrders.First();
+                Entity.WorldUtilityWindowViewModel.SetTimer(first.RemainingSeconds, first.TotalSeconds, "Processing!");
+            }
+        }
+
+        private void ShowUI()
+        {
+            Entity.WorldUtilityWindowViewModel.SetVisible(true);
+        }
+        
+        private void HideUI()
+        {
+            Entity.WorldUtilityWindowViewModel.SetVisible(false);
+        }
+    }
+}
